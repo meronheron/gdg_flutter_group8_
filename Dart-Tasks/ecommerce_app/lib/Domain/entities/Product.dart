a9ad6db9ccfcf8dbaed81ff18dce277eb1b6f73e
meronheron <merongetaneh.sel9@gmail.com>
2025-02-08 21:28:55 +0300

    product

---
+++ Dart-Tasks/ecommerce_app/lib/Domain/entities/Product.dart
@@ -0,0 +1,15 @@
+class Product {
+  String name;
+  String id;
+  String imageUrl;
+  String description;
+  double price;
+
+  Product({
+    required this.name,
+    required this.id,
+    required this.imageUrl,
+    required this.description,
+    required this.price,
+  });
+}
