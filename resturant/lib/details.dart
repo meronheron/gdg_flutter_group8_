20432ca055d0e5e4e255dbb34a1f78e86cf48444
meronheron <merongetaneh.sel9@gmail.com>
2025-03-09 14:21:17 +0300

    add to cart

--- resturant/lib/details.dart
+++ resturant/lib/details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -21,12 +20,20 @@ class FoodDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // List of image paths for add-ons
+    final List<String> addOnImages = [
+      'assets/Rectangle 15.png',
+      'assets/Rectangle 17.png',
+      'assets/Rectangle 18.png',
+    ];
+
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 63, 82, 223),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const SizedBox(height: 30), // Adjust the height to move the image lower
+          const SizedBox(
+              height: 30), // Adjust the height to move the image lower
           Container(
             width: 300, // Increased width
             height: 300, // Increased height
@@ -39,7 +46,8 @@ class FoodDetails extends StatelessWidget {
                   width: 220, // Increased shadow size for more visibility
                   height: 220,
                   fit: BoxFit.contain, // Ensures full visibility
-                  opacity: const AlwaysStoppedAnimation(0.8), // Makes shadow more visible
+                  opacity: const AlwaysStoppedAnimation(
+                      0.8), // Makes shadow more visible
                 ),
                 Image.asset(
                   'assets/159187-burger-double-cheese-free-png-hq 2.png',
@@ -65,15 +73,15 @@ class FoodDetails extends StatelessWidget {
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 20),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 6, horizontal: 20),
                         decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 85, 70, 228),
+                          color: const Color.fromARGB(255, 63, 82, 223),
                           borderRadius: BorderRadius.circular(25),
                         ),
                         child: const Text(
@@ -98,8 +106,104 @@ class FoodDetails extends StatelessWidget {
                       ),
                     ],
                   ),
+                  const SizedBox(height: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Beef Burger',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 5, 5, 5),
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Big juicy burger with Cheese, Lettuce,Onions,\nTomato and special sauce!',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color.fromARGB(255, 5, 5, 5),
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Add Ons:',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color.fromARGB(255, 5, 5, 5),
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: List.generate(addOnImages.length, (index) {
+                          return Stack(
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                  borderRadius: BorderRadius.circular(8),
+                                  image: DecorationImage(
+                                    image: AssetImage(addOnImages[
+                                        index]), // Use different images
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Container(
+                                  width: 18,
+                                  height: 18,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.green,
+                                  ),
+                                  child: const Icon(
+                                    Icons.add,
+                                    size: 14,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+                        ), 
+                      ),
+                      const SizedBox(height: 30),
+                       Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 63, 82, 223),
+                        padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Text(
+                        'Add to Cart',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                    ),
+                  ),
+                    ],
+                  ),
                 ],
-              ),    
+              ),
             ),
           ),
         ],
